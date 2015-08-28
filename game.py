61695a9a504b2e303116301927a3176f4d8e5dab
Andrew Stone <astone@debian>
2015-08-28 19:49:59 -0400

    Bobo responds

--- game.py
+++ game.py
@@ -1,12 +1,31 @@
-from random import random
+from random import random, randint, sample
 
 boboStates = ['no', 'yes']
 boboResponses = ['Bobo says %s%s' % (x,x) for x in boboStates]
-
-def randInt(max):
-    return (int)(random() * (max+1))
+boboActivities = {
+    "n": 
+        [
+        "scratches his butt",
+        "throws some poo at you",
+        "gives you the hairy eyeball",
+        "looks generally annoyed"
+        ],
+    "y":
+        ["sings Ave Maria",
+         "gives you a flower",
+         "recites a beautiful Shakespeare sonnet",
+         "cures cancer"
+        ]
+}
 
 while True:
     print "What would you like to say to Bobo?"
     question = raw_input()
-    print boboResponses[randInt(len(boboResponses)-1)]
+    print boboResponses[randint(0,len(boboResponses)-1)]
+
+    youLikey = 'x'
+    while youLikey not in 'yn' :
+        print "Do you like Bobo's response? [y/n]"
+        youLikey = raw_input().lower()
+
+    print "Fine, Bobo %s"%sample(boboActivities[youLikey],1)[0]
