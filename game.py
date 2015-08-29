ac0ff0804528c1eba24d69f88c7fd9df467709ca
Andrew Stone <astone@debian>
2015-08-28 20:42:47 -0400

    Adam says: what a FUNNY little monkey!

--- game.py
+++ game.py
@@ -1,27 +1,66 @@
+import os
+import time
 from random import random, randint, sample
 
-VERBS = ["smirk", "talk"]
-MISSING_NOUN = {   "smirk": "Smirk at who?"
-                 , "talk": "Talk to who?"}
+def i(s) :
+    def _(*args,**kwargs) :
+        print s
+    return _
+
+def play(*args,**kwargs) :
+    print "Bobo looks at you with childlike excitement!"
+    time.sleep(3)
+    game_width = 25
+    curr_pos = (0,0)
+    win_pos = (randint(0,game_width),randint(0,game_width))
+    while curr_pos != win_pos :
+        board = [['.' for _ in range(game_width)] for __ in range(game_width)]
+        board[curr_pos[1]][curr_pos[0]] = 'B'
+        board[win_pos[1]][win_pos[0]] = 'X'
+        os.system("clear")
+        print '\n'.join(''.join(_) for _ in board)
+        print 'Control Bobo! [jkhl]'
+        move = raw_input()
+        if move not in 'jkhl' :
+            continue
+        elif move == 'j' :
+            curr_pos = (max(0,min(curr_pos[0],game_width)),
+                        max(0,min(curr_pos[1]+1,game_width)))
+        elif move == 'k' :
+            curr_pos = (max(0,min(curr_pos[0],game_width)),
+                        max(0,min(curr_pos[1]-1,game_width)))
+        elif move == 'h' :
+            curr_pos = (max(0,min(curr_pos[0]-1,game_width)),
+                        max(0,min(curr_pos[1],game_width)))
+        elif move == 'l' :
+            curr_pos = (max(0,min(curr_pos[0]+1,game_width)),
+                        max(0,min(curr_pos[1],game_width)))
+
+VERB_ACTIONS = {"smirk":i("Bobo smirks back at you"),
+                "talk":i("Bobo is a monkey. He can talk, but he won't talk to you."),
+                "play":play
+               }
+MISSING_NOUN = {   "smirk": i("Smirk at who?")
+                 , "talk": i("Talk to who?")
+                 , "play": i("With yourself?!")
+               }
 
 
 def parseCommand(command):
-    tokens = command.split(' ')
-    if len(tokens) == 0:
-        return None
-    if len(tokens) == 1:
-        verb = tokens[0]
-        if verb in MISSING_NOUN.keys():
-            print MISSING_NOUN[verb]
-        return None
-    return (tokens[0], tokens[1])
+    verb = None
+    for v in VERB_ACTIONS.keys() :
+        if v in command.lower() :
+            verb = v
+            break
 
-def performAction(action):
-    (verb, noun) = action
-    if verb == 'smirk' and noun == 'bobo':
-        print "Bobo smirks back at you."
-    elif verb == 'talk' and noun == 'bobo':
-        talkToBobo()
+    if verb is None :
+        return None
+
+    if "bobo" not in command.lower() :
+        print MISSING_NOUN[verb]()
+
+    return verb
+
 
 def talkToBobo():
     boboStates = ['no', 'yes']
@@ -59,16 +98,15 @@ def talkToBobo():
 def gameLoop():
     while True:
         print
-        print "You can <verb> <noun>.  Valid verbs include: %s" % ', '.join(VERBS)
+        print "You are in a room with Bobo. There is nothing else but Bobo."
+        print "You can do things to Bobo.  Valid things include: %s" % ', '.join(VERB_ACTIONS.keys())
         print '>', 
         command = raw_input()
         print
-        r = parseCommand(command)
-        if r == None:
+        action = parseCommand(command)
+        if action == None:
             continue
-        performAction(r)
-        
+        VERB_ACTIONS[action]()
 
 
 gameLoop();
-
