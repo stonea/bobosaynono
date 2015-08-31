c37d68054c1228790c95dc1d16ad62ac31913f2d
Andrew Stone <astone@debian>
2015-08-31 10:42:26 -0400

    Take out debugging cruft

--- gamestate.py
+++ gamestate.py
@@ -2,7 +2,6 @@ from util import *
 
 _gameRooms = {}
 _currentRoomName = 'boboRoom'
-_currentRoomName = 'secretRoom' #####
 _inventory = {'monies':10}
 _suckedIt = 0
 _unMetAchievements = ['gotSword', 'gotDong', 'playCarnival', 'spicyRoom', 'deFuca']
