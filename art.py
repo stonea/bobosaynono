8bc06835c834e1a0148c9526e23d558fc2653c2a
Andrew Stone <astone@debian>
2015-09-03 21:23:57 -0400

    Move room json files into rooms subdir.  Refactor enemy stats into json file.  Add cool battle graphixgit statusgit statusgit status111

---
+++ art.py
@@ -0,0 +1,62 @@
+KITTY_WITTY = \
+r"""
+  (.   \                    
+    \  |                    
+     \ |___(\--/)           
+   __/    (  . . )          
+  "'._.    '-.O.'           
+       '-.  \ "|\           
+          '.,,/'.,,mrf      """
+KITTY_WITTY_CREDIT = "Art found at: <http://user.xmission.com/~emailbox/ascii_cats.htm>"
+
+WUMPUS = KITTY_WITTY
+LADYBUG = KITTY_WITTY
+BARREL = KITTY_WITTY
+
+
+CHTHULUH = \
+r"""
+   ________                 
+  /         \               
+  |          \              
+  |           \             
+  |            \            
+  \        __\/_\           
+   \       \_/\_/\___       
+    \/  / |  \ | \   \      
+   _/ _/ _|  | \  \  |_     
+  /  |  / /  / /  |  \ \_   
+  | /  /  | |  \   \  \  \ """
+CTHULU_CREDIT = 'Art by: "Spurious Logik (Sir)" Found on alt.horror.cthulhu'
+
+WUMPUS = \
+r"""
+  \\ (.)  (.) //      
+   \\  //\\  //       
+    \\//  \\//        """
+WUMPUS_CREDIT = "Art hand-crafted by BoBo"
+
+LADYBUG = \
+r"""
+        _ .-. _     
+       /o`\^/`o\    
+      |o o | o o|   
+       \o _|_ o/    
+        `(@I@)`     
+          /^\       """
+LADY_BUG_CREDIT = "http://www.retrojunkie.com/asciiart/animals/ladybugs.htm"
+
+
+BARREL = \
+r"""
+    .----,      
+   |      |     
+   |      |     
+   |`----'|     
+   |      |     
+   |      |     
+   |      |     
+   |      |     
+   `------'     """
+BARREL_CREDIT = "Art hand-crafted by BoBo"
+
